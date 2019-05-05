8554bd48537e83754dc0946df0756ca812329deb
Kirby <kirby.hackett@dal.ca>
2019-05-05 01:13:04 -0300

    added favicon and watcher for radio buttons

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+var rows = 2;
+var columns = 3;
+
 // click event for the generate template button
 $('#generateTemplate').click((e)=>{
     // this code will execute once generate template is clicked
     e.preventDefault();
+    console.log(rows + "  " + columns)
+    
+    for(var i = 0; i < rows; i++) {
+        for(var j = 0; j < columns; j++) {
+            console.log('*');
+        }
+        console.log('\n');
+    }
+});
+
+$('.form-check-input').click((e)=>{
+    var value = e.currentTarget.value.slice(-1);
+    if(e.currentTarget.value.startsWith('row')) rows = value;
+    else columns = value;
 });
 
 $( ".custom-range" ).change(function(e) {
