04f8ef3e7c453a70f66f2d355cd5a7dca526bb9b
Kirby <kirby.hackett@dal.ca>
2019-05-05 13:21:13 -0300

    dynamic html tables and renamed files

--- main.js
+++ main.js
@@ -1,27 +1,50 @@
+// default global variables
 var rows = 2;
 var columns = 3;
+var spacing = 3;
+var htmlTable = '';
+var ampTable = ''; 
 
 // click event for the generate template button
 $('#generateTemplate').click((e)=>{
+    
     // this code will execute once generate template is clicked
     e.preventDefault();
-    console.log(rows + "  " + columns)
+    
+    htmlTable = `<table class="dh-gallery-table">`;
     
     for(var i = 0; i < rows; i++) {
+        htmlTable = htmlTable + `<tr>`;
+        
         for(var j = 0; j < columns; j++) {
-            console.log('*');
+            htmlTable = htmlTable + `
+              <td style="padding:` + spacing + `px;">
+                <a href="https://www.hackett.dev">
+                  <img src="https://placeimg.com/100/100/animals" height="100" width="100" />
+                </a>
+              </td>`;
         }
-        console.log('\n');
+        htmlTable = htmlTable + `</tr>`;
     }
+    htmlTable = htmlTable + `</table>`;
+    document.getElementById('table-preview').innerHTML = htmlTable;
 });
 
+// radio button click event
 $('.form-check-input').click((e)=>{
     var value = e.currentTarget.value.slice(-1);
-    if(e.currentTarget.value.startsWith('row')) rows = value;
-    else columns = value;
+    if(e.currentTarget.value.startsWith('row')) {
+        rows = value;
+        document.getElementById('rowsValue').innerHTML = rows;
+    } 
+    else {
+        columns = value
+        document.getElementById('columnsValue').innerHTML = columns;
+    };
 });
 
+// slider change event
 $( ".custom-range" ).change(function(e) {
+    spacing = e.currentTarget.value;
     document.getElementById('spacingValue').innerHTML = e.currentTarget.value + 'px';
-  console.dir(document.getElementById("spacingValue").value);
 });
